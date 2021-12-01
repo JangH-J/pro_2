c10ea7aea79d3093f42b146477821229131912b5
JANG <hjj19948201@gmail.com>
2021-12-01 14:44:40 +0900

    123

--- pro_2/src/main/java/kr/co/pro_2/manage_controller/ManageController.java
+++ pro_2/src/main/java/kr/co/pro_2/manage_controller/ManageController.java
@@ -1,12 +1,22 @@
 package kr.co.pro_2.manage_controller;
 
-import org.apache.ibatis.session.SqlSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.pro_2.manage_service.ManageService;
 
 @Controller
 public class ManageController {
 	
 	@Autowired
-	SqlSession sqlSession;
+	@Qualifier("ms")
+	private ManageService service;
+	
+	@RequestMapping("manage_index")
+	public String manage_index() {
+		return "manage_index()";
+	}
 }
