399acf07627659e8b2b0fa52abcd8d083110a78e
JANG <hjj19948201@gmail.com>
2021-12-01 14:37:44 +0900

    update

--- pro_2/src/main/java/kr/co/pro_2/manage_controller/ManageController.java
+++ pro_2/src/main/java/kr/co/pro_2/manage_controller/ManageController.java
@@ -1,9 +1,12 @@
 package kr.co.pro_2.manage_controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ManageController {
 	
 	@Autowired
+	SqlSession sqlSession;
 }
