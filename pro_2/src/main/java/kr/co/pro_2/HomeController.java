4e3f86ddee4692ceefc14ce2fbfe20aab09327c6
NUC <NUC@OOHeo>
2021-12-03 16:24:59 +0900

    homecontroller 수정

--- pro_2/src/main/java/kr/co/pro_2/HomeController.java
+++ pro_2/src/main/java/kr/co/pro_2/HomeController.java
@@ -2,10 +2,7 @@ package kr.co.pro_2;
 
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
