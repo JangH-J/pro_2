0458918f934bb8d8f66e2770198805954b287fa7
hkh87 <hkh87@OOHeo-windowsPC>
2021-12-12 00:13:12 +0900

    허정훈 인덱스,디폴트 일부수정, 디자인 수정, 이미지5개 추가

---
+++ pro_2/src/main/webapp/resources/main_js/main_index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+	function moving_front_ad(){
+		var ad01=document.getElementById("front_ad01");
+		var ad02=document.getElementById("front_ad01");
+		var ad03=document.getElementById("front_ad01");
+		var ad04=document.getElementById("front_ad01");
+		
+		
+			
+	}
